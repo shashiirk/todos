78249f930ea3947f6b5b98f00a8523005053f829
Shashikanth Reddy <kolanshashii@gmail.com>
2021-04-27 16:11:50 +0530

    used useEffect for localStorage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './components/Header/Header';
 import UserInput from './components/UserInput/UserInput';
 import TasksList from './components/TasksList/TasksList';
 import './App.css';
 
 const App = () => {
-  const [tasksList, setTasksList] = useState(
-    JSON.parse(localStorage.getItem('tasks')) || []
-  );
+  const [tasksList, setTasksList] = useState([]);
+
+  useEffect(() => {
+    const localItems = JSON.parse(localStorage.getItem('tasks'));
+    if (localItems) {
+      setTasksList(localItems);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (tasksList.length === 0) {
+      localStorage.removeItem('tasks');
+    } else {
+      localStorage.setItem('tasks', JSON.stringify(tasksList));
+    }
+  }, [tasksList]);
 
   const addTaskHandler = (newTask) => {
     setTasksList((prevTasks) => {
       const updatedTasksList = [...prevTasks, newTask];
-      updateLocalStorage(updatedTasksList);
       return updatedTasksList;
     });
   };
@@ -20,7 +32,6 @@ const App = () => {
   const deleteTaskHandler = (taskId) => {
     setTasksList((prevTasks) => {
       const updatedTasksList = prevTasks.filter((task) => task.id !== taskId);
-      updateLocalStorage(updatedTasksList);
       return updatedTasksList;
     });
   };
@@ -28,18 +39,15 @@ const App = () => {
   const updateTaskHandler = (taskId) => {
     setTasksList((prevTasks) => {
       const taskIndex = prevTasks.findIndex((task) => task.id === taskId);
-      prevTasks[taskIndex].status = !prevTasks[taskIndex].status;
-      updateLocalStorage(prevTasks);
-      return prevTasks;
+      const existingTask = prevTasks[taskIndex];
+      const updatedTask = {
+        ...existingTask,
+        status: !existingTask.status,
+      };
+      const updatedTasksList = [...prevTasks];
+      updatedTasksList[taskIndex] = updatedTask;
+      return updatedTasksList;
     });
-  };
-
-  const updateLocalStorage = (newTasksList) => {
-    if (newTasksList.length === 0) {
-      localStorage.removeItem('tasks');
-    } else {
-      localStorage.setItem('tasks', JSON.stringify(newTasksList));
-    }
   };
 
   return (
